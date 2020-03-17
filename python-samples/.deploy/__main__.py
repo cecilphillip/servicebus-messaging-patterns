4e036ed345371aa320052e88217d2893c5b531b7
cecilphillip <cecilphillip@gmail.com>
2020-03-17 21:46:58 +0000

    add subscriptions and file to deployment

--- python-samples/.deploy/__main__.py
+++ python-samples/.deploy/__main__.py
@@ -1,7 +1,6 @@
 import pulumi
 from isodate import Duration, duration_isoformat
-from pulumi_azure import core, storage, servicebus
-
+from pulumi_azure import core, cosmosdb, appservice, storage, servicebus
 
 # Create an Azure Resource Group
 resource_group = core.ResourceGroup(
@@ -27,7 +26,68 @@ servicebus.Queue('simplequeue',
                  default_message_ttl=duration_isoformat(Duration(days=5))
                  )
 
+# Create an associated topic within the above namespace
+simpletopic = servicebus.Topic('simpletopic',
+                               name='simpletopic',
+                               resource_group_name=resource_group.name,
+                               namespace_name=sbnamespace.name,
+                               max_size_in_megabytes=1024,
+                               status='Active'
+                               )
+
+servicebus.Subscription('all_messages',
+                        name='all_messages',
+                        resource_group_name=resource_group.name,
+                        namespace_name=sbnamespace.name,
+                        max_delivery_count=5,
+                        dead_lettering_on_message_expiration=True,
+                        default_message_ttl=duration_isoformat(
+                            Duration(days=5)),
+                        requires_session=False,
+                        topic_name=simpletopic.name)
+
+filtered_messages = servicebus.Subscription('filtered_messages',
+                                            name='filtered_messages',
+                                            resource_group_name=resource_group.name,
+                                            namespace_name=sbnamespace.name,
+                                            max_delivery_count=5,
+                                            dead_lettering_on_message_expiration=True,
+                                            default_message_ttl=duration_isoformat(
+                                                Duration(days=5)),
+                                            requires_session=False,
+                                            topic_name=simpletopic.name)
+
+servicebus.SubscriptionRule('content_filter',
+                            name='content_filter',
+                            topic_name=simpletopic.name,
+                            subscription_name=filtered_messages.name,
+                            resource_group_name=resource_group.name,
+                            namespace_name=sbnamespace.name,
+                            filter_type='CorrelationFilter',
+                            correlation_filter={
+                                'content_type': 'application/json',
+                                # 'label': 'content'
+                            })
+
+# Create sender and receiver access policies
+receiverKey = servicebus.NamespaceAuthorizationRule('RootReceiverAccessKey',
+                                                    listen=True,
+                                                    send=False,
+                                                    namespace_name=sbnamespace.name,
+                                                    resource_group_name=resource_group.name,
+                                                    name='RootReceiverAccessKey')
+
+senderKey = servicebus.NamespaceAuthorizationRule('RootSenderAccessKey',
+                                                  listen=False,
+                                                  send=True,
+                                                  namespace_name=sbnamespace.name,
+                                                  resource_group_name=resource_group.name,
+                                                  name='RootSenderAccessKey')
+
 
 # Export the connection string for the storage account
 pulumi.export('namespace_id', sbnamespace.id)
 pulumi.export('namespace_name', sbnamespace.name)
+pulumi.export('sender connection string', senderKey.primary_connection_string)
+pulumi.export('receiver connection string',
+              receiverKey.primary_connection_string)
